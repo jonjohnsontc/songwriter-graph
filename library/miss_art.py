ffa4a2a235d38f4af05d95d0e16189b8b6192d94
Jon Johnson <jonjohnsontc@live.com>
2018-10-28 10:04:19 -0700

    adding library

--- library/miss_art.py
+++
@@ -1,9 +0,0 @@
-def miss_art(conf_list):
-    '''
-    Iterate through `confident_list` for any artists that weren't retrieved during the lists' creation
-    '''
-    missing_artists = []
-    for i in list(enumerate(confident_list)):
-        if i[1] == 'no_results':
-            missing_artists.append((i[0], artist_list_td[i[0]]))
-    return missing_artists
