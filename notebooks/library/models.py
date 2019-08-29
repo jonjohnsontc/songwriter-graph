9141ce1cf4e8a6de0911267b1cc8891feb8efa95
Jon Johnson <jonjohnsontc@live.com>
2019-08-28 20:50:34 -0700

    building models.py + pre run 8/25

--- notebooks/library/models.py
+++ notebooks/library/models.py
@@ -2,9 +2,24 @@ import numpy as np
 import pandas as pd 
 
 import dask.dataframe as dd
-from dask.distributed import Client 
+from dask.distributed import Client
+from dask_ml.wrappers import Incremental
 
-from sklearn.metrics import jaccard_score
 
 def get_jaccard():
     pass
+
+
+if __name__ == "__main__":
+    modeling_ready_ddf = dd.read_csv("../data/modeling/scaled_ddf_20190825.csv")\
+                           .rename(columns = {"Unnamed: 0":"index"})\
+                           .set_index('index')
+    labels_ddf = dd.read_csv("../data/modeling/metadata_for_scaled_ddf_2019825")\
+                   .rename(columns = {"Unnamed: 0":"index"})\
+                   .set_index('index')
+    
+    
+
+    df_indices.to_csv("")
+    df_distances.to_csv("")
+    return "Finished"
