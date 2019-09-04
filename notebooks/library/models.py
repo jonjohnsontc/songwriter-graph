62355f77a08ebda3b4716332d90f8a421926986d
Jon Johnson <jonjohnsontc@live.com>
2019-09-04 02:42:39 +0000

    further refactoring of models

--- notebooks/library/models.py
+++ notebooks/library/models.py
@@ -1,3 +1,5 @@
+import argparse
+from datetime import datetime
 import os
 import pickle as pkl
 
@@ -5,81 +7,79 @@ import numpy as np
 import pandas as pd 
 import dask.dataframe as dd
 from dask.distributed import Client
-from dask_ml.wrappers import Incremental
 import s3fs
 
 import joblib
 from sklearn.neighbors import NearestNeighbors
 
 from library.config import modeling_labels_path, modeling_path
-from library.utils import find_latest_file, load_dd
+from library.utils import find_latest_file, find_latest_file_s3, load_dd, load_df
 
+TIME_AT_RUN = datetime.utcnow()
 
 
 def get_euclidean(modeling_ready_ddf):
-    modeling_ready_ddf = dd.read_csv(find_latest_file(modeling_path))\
-                           .rename(columns = {"Unnamed: 0":"index"})\
-                           .set_index('index')
     print("Fitting Model")
-    nn = NearestNeighbors(n_neighbors=10, metric="euclidean").fit()
-    nn.fit()
-    nn_incremental = Incremental(nn)
-    fit_ddf = nn_incremental.fit(modeling_ready_ddf)
+
+    #TODO: Look at joblib for potential parallelism across operations
+    #https://joblib.readthedocs.io/en/latest/generated/joblib.Parallel.html#joblib.Parallel
+    nn = NearestNeighbors(n_neighbors=10, metric="euclidean", n_jobs=-1)
+   
+    #NOTE: Formerly used "Incremental", but `n_jobs` controls parallelism
+    nn.fit(modeling_ready_ddf)
 
     print("Retrieving Neighbors")
     distances, indices = nn.kneighbors(modeling_ready_ddf)
     
     print("Saving Neighbors to s3")
     # https://stackoverflow.com/a/44818887
-    fs = s3fs.S3FileSystem(key=os.path.get('AWS_KEY'), secret=os.path.get('AWS_SECRET_KEY'))
-    with fs.open('s3://swg_bucket/processed/distances/distances_{}.pkl', 'wb') as f:
+    fs = s3fs.S3FileSystem()
+    with fs.open(f's3://swg_bucket/processed/distances/distances_{TIME_AT_RUN}.pkl', 'wb') as f:
         pkl.dump(distances, f)
-    with fs.open('s3://swg_bucket/processed/indices/indices_{}.pkl', 'wb') as f:
+    with fs.open(f's3://swg_bucket/processed/indices/indices_{TIME_AT_RUN}.pkl', 'wb') as f:
         pkl.dump(indices, f)
+    
+    print("Finished")
+    return distances, indices
+
+
+def create_model_df(labels, matrix, type_of_file):
+    """Takes in song labels, along with distances and indices of neighbors
+    to produce a more easily interpretable dataset
+
+    Args:
+        labels: Pandas DataFrame, a dataframe of label values
+        matrix: Numpy ndarray of either distances or indices computed
+                from some NearestNeighbors modeling method
+        type_of_file: str, either "distances" or "indices" for now,
+                      although this could change with other models
+                      being constructed.
+
+    Returns:
+        "Finished" Message, after saving 
+    """
+    ms = matrix.shape[0]
+    df_matrix = pd.DataFrame(np.vstack(np.split(matrix, ms)))
+    labeled_matrix = pd.concat([labels,df_matrix])
+
+    print("Saving Files")
+    df_matrix_in_bytes = labeled_matrix.to_csv(None).encode()
+    fs = s3fs.S3FileSystem()
+    with fs.open(f's3://swg_bucket/processed/{type_of_file}/{type_of_file}_df_{TIME_AT_RUN}.csv', 'wb') as f:
+        f.write(df_matrix_in_bytes)
     return "Finished"
+
+def parse_args():
+    parser = argparse.ArgumentParser('Description: Generate models based on songwriter data passed through')
+    parser.add_argument(
+        "path",
+        help="Location of the modeling ready songwriter data",
+        type=str)
+    return vars(parser.parse_args())
+
 
 
 if __name__ == "__main__":
-    ddf = load_dd(find_latest_file(modeling_path))
-
-    # NOTE: Do not need if I don't create DF
-    # labels_ddf = dd.read_csv(find_latest_file(modeling_labels_path))\
-    #                .rename(columns = {"Unnamed: 0":"index"})\
-    #                .set_index('index')
-    
-    print("Fitting Model")
-    nn = NearestNeighbors(n_neighbors=10, metric="euclidean").fit()
-    nn.fit()
-    nn_incremental = Incremental(nn)
-    fit_ddf = nn_incremental.fit(modeling_ready_ddf)
-
-    print("Retrieving Neighbors")
-    distances, indices = nn.kneighbors(modeling_ready_ddf)
-
-    #TODO: .ENV file
-    print("Saving Neighbors to s3")
-    # https://stackoverflow.com/a/44818887
-    fs = s3fs.S3FileSystem(key=os.path.get('AWS_KEY'), secret=os.path.get('AWS_SECRET_KEY'))
-    with fs.open('s3://swg_bucket/processed/distances/distances_{}.pkl', 'wb') as f:
-        pkl.dump(distances, f)
-    with fs.open('s3://swg_bucket/processed/indices/indices_{}.pkl', 'wb') as f:
-        pkl.dump(indices, f)
-    
-    #NOTE: Toss in separate function
-    # print("Building Labeled DataFrames for Neighbor Indices and Distances")
-    # ds = distances.shape[0]
-    # df_distances = pd.DataFrame(np.vstack(np.split(distances, ds)))
-    # df_indices = pd.DataFrame(np.vstack(np.split(indices, ds)))
-    # labeled_distances = pd.concat([labels_ddf,
-    #                                df_distances])
-    # labeled_indices = pd.concat([labels_ddf,
-    #                              df_indices])
-
-    # print("Saving Files")
-    # df_indices_in_bytes = labeled_indices.to_csv(None).encode()
-    # with fs.open('s3://swg_bucket/processed/indices/indices_df_{}.csv', 'wb') as f:
-    #     f.write(indices)
-    # df_distances_in_bytes = labeled_distances.to_csv(None).encode()
-    # with fs.open('s3://swg_bucket/processed/distances/distances_df_{}.csv', 'wb') as f:
-    #     f.write(distances)
-    return "Finished"
+    args = parse_args()
+    ddf = load_dd(find_latest_file_s3(args['path']))
+    dist, indices = get_euclidean(ddf)
--- notebooks/library/utils.py
+++ notebooks/library/utils.py
@@ -1,11 +1,30 @@
 import glob
 import os
 
+import s3fs
+
 from dask import dataframe as dd
+import pandas as pd
+
+def find_latest_file_s3(path):
+    """Finds the latest file in an s3 path, based off of a glob string
+    passed in 'path'
+
+    Returns:
+        String path leading to the latest file in a directory
+    """
+    fs = s3fs.S3FileSystem()
+    list_of_files = fs.glob(path)
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return latest_file
+
 
 def find_latest_file(path):
     """Finds the latest file in a path, based off of a glob string passed
     in 'path'
+
+    Returns:
+        String path leading to the latest file in a directory
     """
     # glob hint https://stackoverflow.com/a/39327156
     list_of_files = glob.glob(path)
@@ -18,3 +37,8 @@ def load_dd(path):
             .rename(columns = {"Unnamed: 0":"index"})\
             .set_index('index')
     return ddf
+
+
+def load_df(path):
+    df = pd.read_csv(path, index_col=0)
+    return df
