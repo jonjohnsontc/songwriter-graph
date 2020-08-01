0aa0b4462c992c804a7827f4fa5b97c6fd337a77
Jon Johnson <jonjohnsontc@live.com>
2020-07-31 19:38:42 -0700

    basic hello world to validate html

---
+++ songwriter_graph/api/api.py
@@ -0,0 +1,14 @@
+import time
+from flask import Flask
+from flask_restx import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
