94d1f7b7c6710b7e37837c3add4a61fe17f62000
Jon Johnson <jonjohnsontc@live.com>
2021-07-01 19:09:43 -0700

    removed a bunch of top-level files in songwriter_graph + pickle files

--- songwriter_graph/genre_psong.py
+++
@@ -1,9 +0,0 @@
-def genre_psong(genres, songs):
-    gs_list = []
-    count = 1
-    for song, artist in songs[["s_song_id", "artist_id"]].values:
-        for genre in genres:
-            if artist == genre[1]:
-                gs_list.append((count, song, genre[0], genre[1]))
-                count += 1
-    return gs_list
