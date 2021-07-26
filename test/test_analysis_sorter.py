2bace9eb3271382388491cdd45453c309f1af42f
Jon Johnson <jonjohnsontc@live.com>
2021-07-25 17:09:15 -0700

    yay first passing tests

--- test/test_analysis_sorter.py
+++ test/test_analysis_sorter.py
@@ -1,36 +1,34 @@
 import json
 
-from songwriter_graph.analysis_sorter import get_mean_var
+from songwriter_graph.analysis_sorter import get_mean_var, get_key_changes
 from songwriter_graph.utils import save_object_np
 import pytest
 
 import numpy as np
 import pandas as pd
 
-@pytest.fixture
+
+# TODO: Don't think this is worth testing -- just concats np.mean + np.var
+def test_get_mean_var_returns_mean_and_variance():
+    pass
 
 
-
-#TODO: Add some actual test here
-# currently takes an assload of time to run
-# def test_get_mean_var_returns_mean_and_variance():
-#     test_file = glob.glob("/home/jovyan/SWI_data/data/audio_analysis/*.json")
-#     with open(file_listing[0], 'r') as f:
-#         test_json_obj = json.load(f)
-#     test_mean_var = get_mean_var(test_json_obj)
-#     assert len(test_mean_var) > 5
-
-
+# TODO: Should I be doing this? Do I need to test PCA? Can I test it?
 def test_get_pt_pca_produces_accurate_pca():
     pass
 
 
+# TODO: Hasn't been implemented yet
 def test_PCA_result_can_map_back_to_original_data():
     pass
 
 
-def get_key_changes_produces_accurate_no():
-    pass
+def test_get_key_changes_produces_accurate_no():
+    # Should change 7 times
+    keys = np.array([0, 3, 4, 8, 0, 7, 9, 4, 4])
+    expected = 7
+    actual = get_key_changes(keys)
+    assert actual == expected
 
 
 def validate_analysis_obj_properly_throws_exception():
